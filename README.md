ca892824e83e00ccd64c3431e3bfaced5c5e51d4
alfinkerin <kerinalfin70@gmail.com>
2021-02-06 19:57:26 +0700

    boilerplate

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# boilerplate-Nextjs
+BoilerPlate NextJs dan tailwind.
+
+Males Setting terus
+
+## Getting Started
+
+Firts, install npm
+
+```npm
+npm install
+```
+
+Second, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+```
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-blue-400 bg-red-400">Halo ini next js with tailwind</p>
+    </div>
+  );
+}
